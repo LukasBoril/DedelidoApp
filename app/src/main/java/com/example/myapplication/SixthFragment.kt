d0ebbf1484786a9b0a86ed9b961c900040d3fb9e
Nadine <dussnad1@students.zhaw.ch>
2021-06-12 12:04:15 +0200

    inital fragments 6 & 7 added

--- app/src/main/java/com/example/myapplication/SixthFragment.kt
+++ app/src/main/java/com/example/myapplication/SixthFragment.kt
@@ -1,30 +1,39 @@
 package com.example.myapplication
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.myapplication.databinding.FragmentFifthBinding
+import com.example.myapplication.databinding.FragmentSixthBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class SixthFragment : Fragment() {
 
-    private var _binding: FragmentFifthBinding? = null
+    private var _binding: FragmentSixthBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val currentPlayer : String = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFifthBinding.inflate(inflater, container, false)
+        getActivity()?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        _binding = FragmentSixthBinding.inflate(inflater, container, false)
+
+//get player
+
+        binding.buttonContinueF6.text = "CONTINUE"
+        binding.buttonExitF6.text = "EXIT"
+
         return binding.root
 
     }
@@ -32,8 +41,8 @@ class SixthFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.buttonContinueF6.setOnClickListener {
+            findNavController().navigate(R.id.
         }
     }
 
