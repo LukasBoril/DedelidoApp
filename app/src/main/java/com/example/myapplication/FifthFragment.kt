890e3066ce276bc8732ed38b12c46416a6df560b
LukasBoril <lukas.boril@gmail.com>
2021-06-12 15:25:49 +0200

    fragment 4%5 initial commit - buttons implemented - navigation implemented - timer's implemented

--- app/src/main/java/com/example/myapplication/FifthFragment.kt
+++ app/src/main/java/com/example/myapplication/FifthFragment.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -32,9 +34,35 @@ class FifthFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        // Set view to landscape
+        getActivity()?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        // Binding of the button and setting it up
+        binding.f5Button.setOnClickListener {
+            findNavController().navigate(R.id.action_fifthFragment_to_fourthFragment)
         }
+
+        // Binding of the text field for the timer
+        var lefttime = binding.f5TimerTextView
+
+        // Setting up a timer that counts down from 10
+        var timePassed= 0
+        val timer = object: CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                lefttime.text = (5-timePassed).toString()
+                timePassed++
+            }
+
+            override fun onFinish() {
+                timePassed= 0
+                view?.post {
+                    findNavController().navigate(R.id.action_fifthFragment_to_fourthFragment)
+                }
+            }
+        }
+        timer.start()
+
+
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/myapplication/FourthFragment.kt
+++ app/src/main/java/com/example/myapplication/FourthFragment.kt
@@ -1,11 +1,16 @@
 package com.example.myapplication
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.example.myapplication.databinding.FragmentFourthBinding
 
 /**
@@ -32,13 +37,50 @@ class FourthFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        // Set view to landscape
+        getActivity()?.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+        // Binding of the button
+        binding.f4ButtonBackToMain.setOnClickListener {
+            findNavController().navigate(R.id.action_fourthFragment_to_FirstFragment)
         }
+
+        // Binding of the text field for the timer
+////// Question: if I do this in the onTick method directly, it does not work... why?
+        var lefttime = binding.f4TextView3
+
+
+        // Setting up a timer that counts down from 10
+        var timePassed= 0
+        val timer = object: CountDownTimer(10000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                lefttime.text = (10-timePassed).toString()
+                timePassed++
+            }
+
+            override fun onFinish() {
+                timePassed= 0
+                view?.post {
+                    findNavController().navigate(R.id.action_fourthFragment_to_fifthFragment)
+                }
+            }
+        }
+        timer.start()
+
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+    fun Fragment.findNavControllerSafely(): NavController? {
+        return if (isAdded) {
+            findNavController()
+        } else {
+            null
+        }
+    }
+
 }
