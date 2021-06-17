19fb736654b140afef96c653b00ff942f0e5fd91
Kaltrim Bajrami <bajrakal@students.zhaw.ch>
2021-06-17 19:54:38 +0200

    commit to branch

--- app/src/main/java/com/example/myapplication/ThirdFragment.kt
+++ app/src/main/java/com/example/myapplication/ThirdFragment.kt
@@ -1,12 +1,24 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import android.widget.ListView
+import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Klaxon
 import com.example.myapplication.databinding.FragmentThirdBinding
+import kotlin.collections.HashMap
+import kotlin.collections.Map
+import kotlin.collections.MutableMap
+import kotlin.collections.set
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -19,6 +31,8 @@ class ThirdFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private var players = mutableListOf<String>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -27,16 +41,49 @@ class ThirdFragment : Fragment() {
         _binding = FragmentThirdBinding.inflate(inflater, container, false)
         return binding.root
 
+
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        val playerList = mutableListOf<String>()
+        binding.buttonAddplayerF3.setOnClickListener {
+            val request = StringRequest(
+                Request.Method.POST,
+                "http://10.0.2.2:8080/players/" + binding.textInputplayersF3.toString(),
+                Response.Listener<String> {
+                },
+                Response.ErrorListener {
+                    //use the porvided VolleyError to display
+                    //an error message
+                    Log.e("ERROR", it.message!!)
+                })
+            requestQueue.add(request)
 
+            //define a request.
+            val getRequest = StringRequest(
+                Request.Method.GET, "http://10.0.2.2:8080/playernames/",
+                Response.Listener<String> { response ->
+                    val board = Klaxon().parse<String>(response)
+                    playerList.add(board!!)
+                },
+                Response.ErrorListener {
+                    //use the porvided VolleyError to display
+                    //an error message
+                    Log.e("ERROR", it.message!! )
+                })
+            requestQueue.add(getRequest)
+        }
 
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 }
+
+
