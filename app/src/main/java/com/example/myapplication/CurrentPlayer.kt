43d7f257c21b4f28bf75207976cde248eba68a9c
LukasBoril <lukas.boril@gmail.com>
2021-06-18 09:10:34 +0200

    Make it run again: - new label for fragment 5 in the nav_graph.xml - rollback of CurrentPlayer.kt: more than one proterty and it crashes

--- app/src/main/java/com/example/myapplication/CurrentPlayer.kt
+++ app/src/main/java/com/example/myapplication/CurrentPlayer.kt
@@ -1,5 +1,11 @@
 package com.example.myapplication
 
 class CurrentPlayer(val name : String) {
+
     fun getPlayerName(): String {return name}
+
+    // public fun getPlayerHealthPoints(): Int {return healthPoints}
+
 }
+
+// , val id: Int, val healthPoints : Int, var yourTurn : Boolean
