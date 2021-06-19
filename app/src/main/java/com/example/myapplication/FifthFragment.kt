c1a1882d61fec371bebd28934c5eb826d18e93f4
LukasBoril <lukas.boril@gmail.com>
2021-06-19 09:48:00 +0200

    Fragment five: - updated all cards to the correct format

--- app/src/main/java/com/example/myapplication/FifthFragment.kt
+++ app/src/main/java/com/example/myapplication/FifthFragment.kt
@@ -3,12 +3,18 @@ package com.example.myapplication
 import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.beust.klaxon.Klaxon
 import com.example.myapplication.databinding.FragmentFifthBinding
 
 /**
@@ -43,21 +49,53 @@ class FifthFragment : Fragment() {
             findNavController().navigate(R.id.action_fifthFragment_to_FourthFragment)
         }
 
+
+
+
         // Binding of the text field for the timer
         var lefttime = binding.f5TimerTextView
 
         // binding of textViews for cards
         var viewCard1 = binding.f5ImageView
-        var id = resources.getIdentifier("com.example.myapplication:drawable/penguin_blue", null, null)
-        viewCard1.setImageResource(id)
+        //var id = resources.getIdentifier("com.example.myapplication:drawable/penguin_blue", null, null)
+        //iewCard1.setImageResource(id)
 
         var viewCard2 = binding.f5ImageView2
-        id = resources.getIdentifier("com.example.myapplication:drawable/penguin_blue", null, null)
-        viewCard2.setImageResource(id)
+        //id = resources.getIdentifier("com.example.myapplication:drawable/penguin_blue", null, null)
+        //viewCard2.setImageResource(id)
 
         var viewCard3 = binding.f5ImageView3
-        id = resources.getIdentifier("com.example.myapplication:drawable/penguin_blue", null, null)
-        viewCard3.setImageResource(id)
+        //id = resources.getIdentifier("com.example.myapplication:drawable/penguin_blue", null, null)
+        //viewCard3.setImageResource(id)
+
+        // getting new cards
+        val requestQueue = Volley.newRequestQueue(requireContext())
+        val request = StringRequest(
+            Request.Method.GET, "http://10.0.2.2:8080/openCards",
+            Response.Listener<String> { response ->
+                val allOpenCards = Klaxon().parse<OpenCards>(response)
+                if (allOpenCards != null) {
+                    var addressFirstCard = produceCardAccessString(allOpenCards.get(0))
+                    var id = resources.getIdentifier("com.example.myapplication:drawable/" + addressFirstCard, null, null)
+                    viewCard1.setImageResource(id)
+
+                    var addressSecondCard = produceCardAccessString(allOpenCards.get(1))
+                    id = resources.getIdentifier("com.example.myapplication:drawable/" + addressSecondCard, null, null)
+                    viewCard2.setImageResource(id)
+
+                    var addressThirdCard = produceCardAccessString(allOpenCards.get(2))
+                    id = resources.getIdentifier("com.example.myapplication:drawable/" + addressThirdCard, null, null)
+                    viewCard3.setImageResource(id)
+                }
+            },
+            Response.ErrorListener {
+                //use the porvided VolleyError to display
+                //an error message
+                Log.e("ERROR", it.message!! )
+            })
+        requestQueue.add(request)
+
+
 
         // Setting up a timer that counts down from 10
         var timePassed= 0
@@ -82,6 +120,13 @@ class FifthFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    fun produceCardAccessString(card : Card): String {
+        var cardColor = card.getCardColor()
+        var cardAnimal = card.getCardAnimal()
+        val output = cardAnimal + "_" + cardColor
+        return output
     }
 
 //
