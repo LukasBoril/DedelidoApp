cf7d69c6db8fa515a057677ba9b21add4143bbd6
Kaltrim Bajrami <bajrakal@students.zhaw.ch>
2021-06-24 23:19:25 +0200

    commit to branch

--- app/src/main/java/com/example/myapplication/ThirdFragment.kt
+++ app/src/main/java/com/example/myapplication/ThirdFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.android.volley.Request
@@ -22,7 +23,9 @@ import kotlin.collections.set
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
- */
+ * */
+
+
 class ThirdFragment : Fragment() {
 
     private var _binding: FragmentThirdBinding? = null
@@ -30,8 +33,9 @@ class ThirdFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private var playerList = mutableListOf<CurrentPlayer>()
+    private var adapter : PlayerAdapter? = null
 
-    private var players = mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -48,11 +52,14 @@ class ThirdFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val requestQueue = Volley.newRequestQueue(requireContext())
-        val playerList = mutableListOf<String>()
+      //  val adapter : ArrayAdapter<CurrentPlayer> = ArrayAdapter<CurrentPlayer>(requireContext(), android.R.layout.simple_list_item_1, android.R.id.text1, playerList);
+       adapter = PlayerAdapter(playerList, requireContext())
+        binding.listPlayersF3.adapter = adapter
+
         binding.buttonAddplayerF3.setOnClickListener {
-            val request = StringRequest(
+            val postRequest = StringRequest(
                 Request.Method.POST,
-                "http://10.0.2.2:8080/players/" + binding.textInputplayersF3.toString(),
+                "http://10.0.2.2:8080/players/" + binding.textInputplayersF3.text.toString(),
                 Response.Listener<String> {
                 },
                 Response.ErrorListener {
@@ -60,21 +67,25 @@ class ThirdFragment : Fragment() {
                     //an error message
                     Log.e("ERROR", it.message!!)
                 })
-            requestQueue.add(request)
+            requestQueue.add(postRequest)
+
 
             //define a request.
             val getRequest = StringRequest(
-                Request.Method.GET, "http://10.0.2.2:8080/playernames/",
+                Request.Method.GET, "http://10.0.2.2:8080/players/",
                 Response.Listener<String> { response ->
-                    val board = Klaxon().parse<String>(response)
-                    playerList.add(board!!)
+                    var players = ArrayList(Klaxon().parseArray<CurrentPlayer>(response))
+                    playerList.addAll(players!!)
+                    adapter?.notifyDataSetChanged()
+
                 },
                 Response.ErrorListener {
                     //use the porvided VolleyError to display
                     //an error message
-                    Log.e("ERROR", it.message!! )
+                    Log.e("ERROR", it.message!!)
                 })
             requestQueue.add(getRequest)
+
         }
 
     }
